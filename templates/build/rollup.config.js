008a6b53b84813503d6a9f1395a21c1a9744890e
levy <levy9527@qq.com>
2021-03-09 19:06:14 +0800

    chore: update depds (#124)

--- templates/build/rollup.config.js
+++ templates/build/rollup.config.js
@@ -1,9 +1,10 @@
 // rollup.config.js
 import vue from 'rollup-plugin-vue'
-import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
+import babel from '@rollup/plugin-babel'
+import commonjs from '@rollup/plugin-commonjs'
 import {terser} from 'rollup-plugin-terser'
 import minimist from 'minimist'
+// import css from 'rollup-plugin-css-only'
 
 const argv = minimist(process.argv.slice(2))
 
@@ -15,15 +16,21 @@ const config = {
   },
   plugins: [
     commonjs(),
+    // https://rollup-plugin-vue.vuejs.org/examples.html#extract-css
+    // css({
+    //   output: '{{componentNamePascal}}',
+    // }),
     vue({
-      css: true,
+      // css: false,
       compileTemplate: true,
       style: {
         postcssPlugins: [require('autoprefixer')]
       }
     }),
     babel({
-      runtimeHelpers: true,
+      // according to the issue, using .babelrc.js can ignore plugins option
+      // https://github.com/rollup/plugins/issues/381
+      babelHelpers: 'runtime',
       extensions: ['.js', '.jsx', '.es6', '.es', '.mjs', '.vue'],
       exclude: 'node_modules/**'
     })
